c9423f2502e139be156553b429ae535cbc59c46a
georgewanyama <wanyamageorge680@gmail.com>
2019-11-10 23:36:40 +0300

    create a function that checks whether the user exists

--- password.py
+++ password.py
@@ -20,3 +20,12 @@ def display_user():
     Function to display existing user
     """
     return User.display_user()
+
+def login_user(username, password):
+    """
+    Function that checks whether a user exists and logs the user in
+    """
+    check_user = Credentials.verify_user(username, password)
+    return check_user
+ 
+ 
